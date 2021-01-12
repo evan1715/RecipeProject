cb83aeb2d3cc1ba7d53171bfdbe09736774b7e3f
Evan <evan1715@users.noreply.github.com>
2021-01-12 10:49:17 -0500

    commented out unused requires

--- server/models/recipeModel.js
+++ server/models/recipeModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
+// const validator = require('validator');
 
 const recipeSchema = new mongoose.Schema({
     title: {

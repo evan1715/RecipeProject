a47502a7fcba24d0d6ecdf97a1eac016436b036c
Adrian Grillo <64506487+AdrianGrillo@users.noreply.github.com>
2021-01-28 17:08:38 -0500

    Add files via upload
    Home page now scales to mobile

--- client/src/components/home-components/HomePage.js
+++ client/src/components/home-components/HomePage.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import CreateAccount from './CreateAccount'
 import RecipeRoulette from './RecipeRoulette'
-// import { useDispatch } from 'react-redux'
-// import { handleRandomRecipes } from '../../actions/random-recipes'
-// import RandomRecipe from './RandomRecipe'
 
 export default function HomePage() {
     return (

84b091aa9af233df1f423aecc764d4f92442dce9
Evan <evan1715@users.noreply.github.com>
2021-04-18 17:55:07 -0400

    Updated index.js to now use async to get the user to load the app.

--- client/src/index.js
+++ client/src/index.js
@@ -5,33 +5,37 @@ import LoadingBar from 'react-redux-loading-bar';
 import AppRouter from './router/AppRouter.js';
 import storeConfig from './store/storeConfig.js';
 import './scss/styles.scss'; //all styles
-import useServerAPI from './hooks/useServerAPI.js';
+import { loginAction } from './actions/account.js';
 
 
 const store = storeConfig();
-const token = localStorage.getItem('token');
 
-//if there is a token stored, check if it's valid from the server.
-if (token) {
-    store.dispatch(useServerAPI('getUser', token));
-}
-
-//Waiting for server to respond if there's a user then loading.
-setTimeout(() => {
+const renderApp = () => {
     ReactDOM.render(
-        <Provider store={store}>
+        <Provider store={ store }>
             <LoadingBar />
             <AppRouter />
         </Provider>,
         document.getElementById('root')
     );
-}, 1000)
+}
 
+const getUser = async () => {
+    const token = localStorage.getItem('token');
 
-// ReactDOM.render(
-//     <Provider store={store}>
-//         <LoadingBar />
-//         <AppRouter />
-//     </Provider>,
-//     document.getElementById('root')
-// );
+    //If there's a token, retrieve user data before rendering the app.
+    if (token) {
+        await fetch('/user/profile', {
+            method: 'GET',
+            headers: { 'Authorization': token }
+        })
+        .then(res => res.json())
+        .then(data => store.dispatch(loginAction(data, token)));
+
+        renderApp();
+    } else { //If there is no token, render the app.
+        renderApp();
+    }
+}
+
+getUser();

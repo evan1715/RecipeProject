cbda53c5c9e13f3769f9bb550bcc96286dba8b67
Evan <evan1715@users.noreply.github.com>
2021-04-23 18:30:38 -0400

    Updated AppRouter to include ViewRecipePage and EditRecipePage. Organized some.

--- client/src/router/AppRouter.js
+++ client/src/router/AppRouter.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Nav from '../components/nav-components/Nav.js';
+import UserRoute from './UserRoute.js'; //private routes
+import Nav from '../components/nav-components/Nav.js'; //nav
+//Home components/non-authorized pages.
 import HomePage from '../components/home-components/HomePage.js';
 import AllRecipesPage from '../components/home-components/AllRecipesPage.js';
 import AboutPage from '../components/home-components/AboutPage.js';
+import ViewRecipePage from '../components/home-components/ViewRecipePage.js';
+//Account components/authorized only pages.
+import EditRecipePage from '../components/account-components/EditRecipePage.js';
 import MyAccountPage from '../components/account-components/MyAccountPage.js';
-import SubmitRecipePage from '../components/account-components/SubmitRecipePage.js';
 import MyRecipesPage from '../components/account-components/MyRecipesPage.js';
-import UserRoute from './UserRoute.js';
+import SubmitRecipePage from '../components/account-components/SubmitRecipePage.js';
 
 const AppRouter = () => (
     <Router>
@@ -25,11 +29,15 @@ const AppRouter = () => (
 
             <Route path={'/about'} component={AboutPage} />
 
-            <UserRoute path='/myaccount' component={ MyAccountPage } />
+            <Route path={`/recipe}`} component={ ViewRecipePage } />
 
-            <UserRoute path='/submitrecipe' component={ SubmitRecipePage } />
+            <UserRoute path='/editrecipe' component={ EditRecipePage } />
+
+            <UserRoute path='/myaccount' component={ MyAccountPage } />
             
             <UserRoute path='/myrecipes' component={ MyRecipesPage } />
+
+            <UserRoute path='/submitrecipe' component={ SubmitRecipePage } />
 
             <Route render={() => <h1>404</h1>} />
         </Switch>

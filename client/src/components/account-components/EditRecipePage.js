33e9dba4ecf621555a2a944061f96e16df9c5ac4
Evan <evan1715@users.noreply.github.com>
2021-04-23 18:27:45 -0400

    Created an EditRecipePage component. Uses RecipeForm component.

---
+++ client/src/components/account-components/EditRecipePage.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import RecipeForm from './RecipeForm.js';
+import recipeServerAPI from '../../database/recipeServerAPI.js';
+
+const EditRecipePage = () => {
+    const dispatch = useDispatch();
+    const userRecipeState = useSelector(state => state.userRecipesReducer);
+
+    //Submit the recipe to the server
+    const onSubmit = (recipe) => {
+        const config = recipe;
+        config.recipe_id = userRecipeState._id;
+
+        dispatch(recipeServerAPI('updateRecipe', config));
+    }
+
+    return (
+        <div>
+            <h1 className="title">Edit Recipe</h1>
+            <RecipeForm
+                onSubmit={ (recipe) => {
+                    onSubmit(recipe)
+                }}
+            />
+        </div>
+    )
+}
+
+export { EditRecipePage as default }

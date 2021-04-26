14090eafbfd1c84ae60ba0fcc6df471ab571b130
Evan <evan1715@users.noreply.github.com>
2021-04-25 21:03:28 -0400

    MyRecipesPage now includes pictures modal, some css, and more.

--- client/src/components/account-components/MyRecipesPage.js
+++ client/src/components/account-components/MyRecipesPage.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import ModifyRecipePicturesModal from './RecipePictures.js';
 import recipeServerAPI from '../../database/recipeServerAPI.js';
 
 const MyRecipesPage = () => {
@@ -9,12 +10,11 @@ const MyRecipesPage = () => {
     const { user, token } = useSelector(state => state.accountReducer);
     const userRecipes = useSelector(state => state.userRecipesReducer);
     const [selectedRecipe, setSelectedRecipe] = useState();
+    const [selectedPictures, setSelectedPictures] = useState([]);
     const [confirmDelete, setConfirmDelete] = useState(false);
     const [approveEditRecipePush, setApproveEditRecipePush] = useState(false);
+    const [openModifyRecipePicturesModal, setOpenModifyRecipePicturesModal] = useState(false);
 
-    //get a recipe by id
-    //update a recipe
-    //delete a recipe
     //upload pictures to recipe
     //delete pictures of recipe
 
@@ -42,24 +42,44 @@ const MyRecipesPage = () => {
     }, [userRecipes])
 
     return (
-        <div>
-            <h1 className="title">My Recipes</h1>
+        <div className="my-recipes-page">
+            <h1 className="title center">My Recipes</h1>
             { userRecipes.length < 1 && <p>You haven't submitted any recipes yet!</p> }
             { userRecipes.length > 0 && userRecipes.map((recipe) => (
-                <div key={ recipe._id }>
-                    <li>{ recipe.title }</li>
-                    <button>View</button>
-                    <button onClick={ () => {
-                        dispatch(recipeServerAPI('getRecipe', recipe._id));
-                        setApproveEditRecipePush(true);
-                    }}>Edit</button>
-                    <button onClick={ () => (setConfirmDelete(true), setSelectedRecipe(recipe._id)) }>Delete</button>
-                    { confirmDelete && (recipe._id === selectedRecipe) && 
-                        <p>
-                            Are you sure?
-                            <button onClick={ () => handleDeleteRecipe(recipe._id) }>Yes</button><button onClick={ () => setConfirmDelete(false) }>No</button>
-                        </p>
-                    }
+                <div className="my-recipes" key={ recipe._id }>
+                    <li className="center" style={{ fontSize: 30 }}>{ recipe.title }</li>
+
+                    <div className="my-recipes-buttons">
+                        <button className="button">View</button>
+
+                        <button className="button" onClick={ () => {
+                            dispatch(recipeServerAPI('getRecipe', recipe._id));
+                            setApproveEditRecipePush(true);
+                        }}>Edit</button>
+
+                        
+                        <button className="button" onClick={ () => {
+                            setOpenModifyRecipePicturesModal(true);
+                            setSelectedRecipe(recipe._id);
+                            setSelectedPictures(recipe.pictures);
+                        }}>Modify pictures</button>
+                        <ModifyRecipePicturesModal
+                            openModifyRecipePicturesModal={ openModifyRecipePicturesModal }
+                            handleCloseModal={ () => setOpenModifyRecipePicturesModal(false) }
+                            recipe_id={ selectedRecipe }
+                            pictures={ selectedPictures }
+                        />
+                        
+
+                        <button className="button" onClick={ () => (setConfirmDelete(true), setSelectedRecipe(recipe._id)) }>Delete</button>
+                        { confirmDelete && (recipe._id === selectedRecipe) && 
+                            <p style={{ fontSize: 30 }}>
+                                Are you sure?
+                                <button className="button" onClick={ () => handleDeleteRecipe(recipe._id) }>Yes</button>
+                                <button className="button" onClick={ () => setConfirmDelete(false) }>No</button>
+                            </p>
+                        }
+                    </div>
                 </div>
             ))}
         </div>

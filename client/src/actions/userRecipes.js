aa4efc66d04ce8887afdc57b18f5d7c8f950f19c
Evan <evan1715@users.noreply.github.com>
2021-04-25 20:55:25 -0400

    Added a function to userRecipes action convert buffer image to binary before placing it in the store.

--- client/src/actions/userRecipes.js
+++ client/src/actions/userRecipes.js
@@ -1,3 +1,24 @@
+const convertBufferToImage = (data) => {
+    let recipes = data;
+
+    for (let i = 0; i < data.length; i++) {
+        const picAmount = data[i].pictures.length;
+
+        for (let j = 0; j < picAmount; j++) {
+            const buffer = data[i].pictures[j].picture.data;
+            const bytes = new Uint8Array(buffer);
+            let binary = '';
+            
+            bytes.forEach((byte) => binary += String.fromCharCode(byte));
+
+            // recipes[i].pictures.splice(j, 1, btoa(binary));
+            recipes[i].pictures[j].picture.data = (btoa(binary));
+            recipes[i].pictures[j].picture.type = "Binary"
+        }
+    }
+    return recipes;
+}
+
 //Submit a recipe
 const submitRecipeAction = (data) => ({
     type: 'SUBMIT_RECIPE',
@@ -5,16 +26,31 @@ const submitRecipeAction = (data) => ({
 });
 
 //ALL_RECIPES
-const allRecipesAction = (recipes) => ({
-    type: 'ALL_RECIPES',
-    recipes
-});
+const allRecipesAction = (data) => {
+    const recipes = convertBufferToImage(data);
+
+    return {
+        type: 'ALL_RECIPES',
+        recipes
+    }
+};
 
 //MY_RECIPES
-const myRecipesAction = (recipes) => ({
-    type: 'MY_RECIPES',
-    recipes
-});
+const myRecipesAction = (data) => {
+    const recipes = convertBufferToImage(data);
+    // const recipes = data;
+    
+    return {
+        type: 'MY_RECIPES',
+        recipes
+    }
+}
+
+// //MY_RECIPES
+// const myRecipesAction = (recipes) => ({
+//     type: 'MY_RECIPES',
+//     recipes
+// });
 
 //GET_RECIPE
 const getRecipeAction = (data) => ({

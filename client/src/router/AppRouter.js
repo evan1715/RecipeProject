f33b0bea69e7f91f90bb42c6d339349361ed61f7
Evan <evan1715@users.noreply.github.com>
2021-04-26 14:10:20 -0400

    Attempted lazy loading in the router. Put it back to regular imports for now.

--- client/src/router/AppRouter.js
+++ client/src/router/AppRouter.js
@@ -2,45 +2,56 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import UserRoute from './UserRoute.js'; //private routes
 import Nav from '../components/nav-components/Nav.js'; //nav
-//Home components/non-authorized pages.
+// Home components/non-authorized pages.
 import HomePage from '../components/home-components/HomePage.js';
 import AllRecipesPage from '../components/home-components/AllRecipesPage.js';
 import AboutPage from '../components/home-components/AboutPage.js';
 import ViewRecipePage from '../components/home-components/ViewRecipePage.js';
+// const HomePage = React.lazy(() => import('../components/home-components/HomePage.js'));
+// const AllRecipesPage = React.lazy(() => import('../components/home-components/AllRecipesPage.js'));
+// const AboutPage = React.lazy(() => import('../components/home-components/AboutPage.js'));
+// const ViewRecipePage = React.lazy(() => import('../components/home-components/ViewRecipePage.js'));
 //Account components/authorized only pages.
 import EditRecipePage from '../components/account-components/EditRecipePage.js';
 import MyAccountPage from '../components/account-components/MyAccountPage.js';
 import MyRecipesPage from '../components/account-components/MyRecipesPage.js';
 import SubmitRecipePage from '../components/account-components/SubmitRecipePage.js';
+// const EditRecipePage = React.lazy(() => import('../components/account-components/EditRecipePage.js'));
+// const MyAccountPage = React.lazy(() => import('../components/account-components/MyAccountPage.js'));
+// const MyRecipesPage = React.lazy(() => import('../components/account-components/MyRecipesPage.js'));
+// const SubmitRecipePage = React.lazy(() => import('../components/account-components/SubmitRecipePage.js'));
+
 
 const AppRouter = () => (
     <Router>
         <Nav />
-        <Switch>
-            <Route exact path={'/'} component={HomePage} />
+        <React.Suspense fallback={<p>Loading...</p>}>
+            <Switch>
+                <Route exact path={'/'} component={HomePage} />
 
-            <Route path={'/allrecipes'} component={ AllRecipesPage } />
+                <Route path={'/allrecipes'} component={ AllRecipesPage } />
 
-            <Route path={'/winepairing'} component={HomePage} />
+                <Route path={'/winepairing'} component={HomePage} />
 
-            <Route path={'/blogposts'} component={HomePage} />
+                <Route path={'/blogposts'} component={HomePage} />
 
-            <Route path={'/cookingvideos'} component={HomePage} />
+                <Route path={'/cookingvideos'} component={HomePage} />
 
-            <Route path={'/about'} component={AboutPage} />
+                <Route path={'/about'} component={AboutPage} />
 
-            <Route path={`/recipe}`} component={ ViewRecipePage } />
+                <Route path={`/recipe`} component={ ViewRecipePage } />
 
-            <UserRoute path='/editrecipe' component={ EditRecipePage } />
+                <UserRoute path='/editrecipe' component={ EditRecipePage } />
 
-            <UserRoute path='/myaccount' component={ MyAccountPage } />
-            
-            <UserRoute path='/myrecipes' component={ MyRecipesPage } />
+                <UserRoute path='/myaccount' component={ MyAccountPage } />
+                
+                <UserRoute path='/myrecipes' component={ MyRecipesPage } />
 
-            <UserRoute path='/submitrecipe' component={ SubmitRecipePage } />
+                <UserRoute path='/submitrecipe' component={ SubmitRecipePage } />
 
-            <Route render={() => <h1>404</h1>} />
-        </Switch>
+                <Route render={() => <h1>404</h1>} />
+            </Switch>
+        </React.Suspense>
     </Router>
 );
 

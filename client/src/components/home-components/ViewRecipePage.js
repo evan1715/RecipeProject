ff8bcfff08fb7910314cfd3e8c25e220e2195956
Evan <evan1715@users.noreply.github.com>
2021-04-26 14:13:20 -0400

    Set up MyRecipesPage to link to /recipe. Added recipe data to ViewRecipePage to make it functional.

--- client/src/components/home-components/ViewRecipePage.js
+++ client/src/components/home-components/ViewRecipePage.js
@@ -1,15 +1,39 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-
+// import { useHistory } from 'react-router';
+import { useSelector } from 'react-redux';
 
 const ViewRecipePage = () => {
-    const dispatch = useDispatch();
-    const userRecipesReducer = useSelector(state => state.userRecipesReducer);
-
+    // const history = useHistory();
+    const userRecipe = useSelector(state => state.userRecipesReducer);
 
     return (
         <div>
+            <h1 className="title center">{ userRecipe.title }</h1>
 
+            <p className="center">Submitted by: pending feature - owner_id: { userRecipe.owner }</p>
+            <p className="center">Submitted: { userRecipe.createdAt }. Last updated: { userRecipe.updatedAt }</p>
+
+            <div className="center">
+                { userRecipe.pictures.map((pic) => (
+                    <img
+                        key={ pic._id }
+                        src={ `data:image/jpeg;base64,${pic.picture.data}` }
+                    />
+                ))}
+            </div>
+
+            <p className="center">Cook time: { userRecipe.cookTime } minutes.</p>
+
+            <ul>
+                <h3 className="title center">Ingredients:</h3>
+                { userRecipe.ingredients.map((ingredient) => (
+                    <li className="center" key={ ingredient._id }>{ ingredient.amount } { ingredient.measurement } of { ingredient.item }</li>
+                ))}
+            </ul>
+            
+            <h3 className="title center">Instructions:</h3>
+            <blockquote className="center">{ userRecipe.instructions }</blockquote>
+            
         </div>
     )
 }

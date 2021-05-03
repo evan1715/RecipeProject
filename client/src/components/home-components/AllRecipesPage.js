659d854851c0ce267bb94be49f28f5e2853c9512
Evan <evan1715@users.noreply.github.com>
2021-05-03 13:11:05 -0400

    Added a view button-link to all recipes page to link to the recipe.

--- client/src/components/home-components/AllRecipesPage.js
+++ client/src/components/home-components/AllRecipesPage.js
@@ -5,6 +5,7 @@ import React, {
 } from 'react';
 // import { useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { showLoading } from 'react-redux-loading-bar';
 import recipeServerAPI from '../../database/recipeServerAPI.js';
 
@@ -21,13 +22,14 @@ const AllRecipesPage = () => {
 
     return (
         <ul>
-            { allRecipes.length > 0 && allRecipes.map((recipe, index) => {
-                return (
-                    <li key={ index }>
-                        <p>Title: { recipe.title } Cook time: { recipe.cookTime } Instructions: { recipe.instructions }</p>
-                    </li>
-                )
-            })}
+            { allRecipes.length > 0 && allRecipes.map((recipe, index) => (
+                <div key={ index } className="row">
+                    <button className="button__link">
+                        <Link className="button__link--Link" to={`/recipe?id=${recipe._id}`}>View</Link>
+                    </button>
+                    <p>Title: { recipe.title } Cook time: { recipe.cookTime } Instructions: { recipe.instructions }</p>
+                </div>
+            ))}
         </ul>
     );
 }

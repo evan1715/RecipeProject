c5c101f3ca013b1afad0c10513afc9922b701665
Evan <evan1715@users.noreply.github.com>
2021-05-05 18:28:09 -0400

    SubmitRecipePage and EditRecipePage now return to /myrecipes when something is submitted. Cleaned up some.

--- client/src/components/account-components/EditRecipePage.js
+++ client/src/components/account-components/EditRecipePage.js
@@ -1,28 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import RecipeForm from './RecipeForm.js';
 import recipeServerAPI from '../../database/recipeServerAPI.js';
 
 const EditRecipePage = () => {
     const dispatch = useDispatch();
-    const userRecipeState = useSelector(state => state.userRecipesReducer);
+    const history = useHistory();
+    const userRecipe = useSelector(state => state.userRecipesReducer);
+    const [approvePush, setApprovePush] = useState(false);
 
     //Submit the recipe to the server
     const onSubmit = (recipe) => {
         const config = recipe;
-        config.recipe_id = userRecipeState._id;
+        config.recipe_id = userRecipe._id;
 
         dispatch(recipeServerAPI('updateRecipe', config));
+        setApprovePush(true);
     }
+
+    useEffect(() => {
+        if (approvePush) {
+            history.push('/myrecipes');
+        }
+    }, [userRecipe]);
 
     return (
         <div>
             <h1 className="title center">Edit Recipe</h1>
-            <RecipeForm
-                onSubmit={ (recipe) => {
-                    onSubmit(recipe)
-                }}
-            />
+            <RecipeForm onSubmit={ (recipe) => onSubmit(recipe) } />
         </div>
     )
 }
--- client/src/components/account-components/SubmitRecipePage.js
+++ client/src/components/account-components/SubmitRecipePage.js
@@ -1,20 +1,28 @@
-import React from 'react';
-// import { useHistory } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import RecipeForm from './RecipeForm.js';
 import recipeServerAPI from '../../database/recipeServerAPI.js';
 
 const SubmitRecipe = () => {
     const dispatch = useDispatch();
+    const history = useHistory();
+    const userRecipe = useSelector(state => state.userRecipesReducer);
+    const [approvePush, setApprovePush] = useState(false);
+
+    useEffect(() => {
+        if (approvePush) {
+            history.push('/myrecipes');
+        }
+    }, [userRecipe]);
 
     return (
         <div>
             <h1 className="title center">Submit a recipe</h1>
-            <RecipeForm
-                onSubmit={ (config) => {
-                    dispatch(recipeServerAPI('submitRecipe', config));
-                }}
-            />
+            <RecipeForm onSubmit={ (config) => {
+                dispatch(recipeServerAPI('submitRecipe', config));
+                setApprovePush(true);
+            }} />
         </div>
     )
 }

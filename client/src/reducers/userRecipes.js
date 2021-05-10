90f727c793eaf21fe41df79d22f886c2f7fa631a
Evan <evan1715@users.noreply.github.com>
2021-05-09 22:44:47 -0400

    Cleaned up userRecipesReducer.

--- client/src/reducers/userRecipes.js
+++ client/src/reducers/userRecipes.js
@@ -3,37 +3,25 @@ const userRecipesReducer = (state = {}, action) => {
         case 'SUBMIT_RECIPE':
             return {
                 ...action.recipe
-            };
+            }
         case 'ALL_RECIPES':
             return [
                 ...action.recipes
-            ]
+            ];
         case 'MY_RECIPES':
             return [
                 ...action.recipes
-            ]
+            ];
         case 'GET_RECIPE':
             return {
                 ...action.recipe
             }
-        case 'UPDATE_RECIPE':
-            return;
         case 'DELETE_RECIPE':
             return {
                 recipe: null
-            };
-        case 'GET_RECIPE_PICTURES':
-            return {
-                ...state,
-                pictures: action.pictures
-            };
-        case 'DELETE_RECIPE_PICTURES':
-            return {
-                ...state,
-                pictures: null
-            };
+            }
         case 'CLEAR_USER_RECIPES':
-            return {};
+            return {}
         default:
             return state;
     }

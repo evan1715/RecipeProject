5748715d1f0f1f43b3b9d3ace91a45afdf8da7e1
Evan <evan1715@users.noreply.github.com>
2021-05-18 11:04:27 -0400

    Renamed home-components folder to home and account-components folder to account.

--- client/src/router/AppRouter.js
+++ client/src/router/AppRouter.js
@@ -3,25 +3,25 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import UserRoute from './UserRoute.js'; //private routes
 import Nav from '../components/nav/Nav.js'; //nav
 // Home components/non-authorized pages.
-// import HomePage from '../components/home-components/HomePage.js';
-// import AllRecipesPage from '../components/home-components/AllRecipesPage.js';
-// import AboutPage from '../components/home-components/AboutPage.js';
-// import ConversionsPage from '../components/home-components/ConversionsPage.js';
-// import ViewRecipePage from '../components/home-components/ViewRecipePage.js';
-const HomePage = React.lazy(() => import('../components/home-components/HomePage.js'));
-const AllRecipesPage = React.lazy(() => import('../components/home-components/AllRecipesPage.js'));
-const AboutPage = React.lazy(() => import('../components/home-components/AboutPage.js'));
-const ConversionsPage = React.lazy(() => import('../components/home-components/ConversionsPage.js'));
-const ViewRecipePage = React.lazy(() => import('../components/home-components/ViewRecipePage.js'));
+// import HomePage from '../components/home/HomePage.js';
+// import AllRecipesPage from '../components/home/AllRecipesPage.js';
+// import AboutPage from '../components/home/AboutPage.js';
+// import ConversionsPage from '../components/home/ConversionsPage.js';
+// import ViewRecipePage from '../components/home/ViewRecipePage.js';
+const HomePage = React.lazy(() => import('../components/home/HomePage.js'));
+const AllRecipesPage = React.lazy(() => import('../components/home/AllRecipesPage.js'));
+const AboutPage = React.lazy(() => import('../components/home/AboutPage.js'));
+const ConversionsPage = React.lazy(() => import('../components/home/ConversionsPage.js'));
+const ViewRecipePage = React.lazy(() => import('../components/home/ViewRecipePage.js'));
 //Account components/authorized only pages.
-// import EditRecipePage from '../components/account-components/EditRecipePage.js';
-// import MyAccountPage from '../components/account-components/MyAccountPage.js';
-// import MyRecipesPage from '../components/account-components/MyRecipesPage.js';
-// import SubmitRecipePage from '../components/account-components/SubmitRecipePage.js';
-const EditRecipePage = React.lazy(() => import('../components/account-components/EditRecipePage.js'));
-const MyAccountPage = React.lazy(() => import('../components/account-components/MyAccountPage.js'));
-const MyRecipesPage = React.lazy(() => import('../components/account-components/MyRecipesPage.js'));
-const SubmitRecipePage = React.lazy(() => import('../components/account-components/SubmitRecipePage.js'));
+// import EditRecipePage from '../components/account/EditRecipePage.js';
+// import MyAccountPage from '../components/account/MyAccountPage.js';
+// import MyRecipesPage from '../components/account/MyRecipesPage.js';
+// import SubmitRecipePage from '../components/account/SubmitRecipePage.js';
+const EditRecipePage = React.lazy(() => import('../components/account/EditRecipePage.js'));
+const MyAccountPage = React.lazy(() => import('../components/account/MyAccountPage.js'));
+const MyRecipesPage = React.lazy(() => import('../components/account/MyRecipesPage.js'));
+const SubmitRecipePage = React.lazy(() => import('../components/account/SubmitRecipePage.js'));
 
 
 const AppRouter = () => (

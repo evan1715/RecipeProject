bb463c9b1d9486da60323f74a2cd09c73bc619f4
Evan <evan1715@users.noreply.github.com>
2021-05-23 13:09:06 -0400

    Added a user profile page and some styling for it. It's still a work in progress.

---
+++ client/src/components/home/UserProfilePage.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { hideLoading, showLoading } from 'react-redux-loading-bar';
+// import userServerAPI from '../../database/userServerAPI.js';
+// import recipeServerAPI from '../../database/recipeServerAPI.js';
+// import processIndividualImage from '../../utils/processIndividualImage';
+
+const UserProfilePage = () => {
+    const dispatch = useDispatch();
+    const location = useLocation();
+    const [username, setUsername] = useState();
+    const [icon, setIcon] = useState();
+    const [userRecipes, setUserRecipes] = useState();
+
+    useEffect(async () => {
+        dispatch(showLoading());
+
+        const user_id = location.search.split('?id=')[1];
+        const user = await (await fetch(`/user/username/${user_id}`)).json();
+        const recipes = await (await fetch(`/recipes/user/${user}`)).json();
+        const blob = await (await fetch(`/user/${user_id}/icon`)).blob();
+        const url = URL.createObjectURL(blob);
+
+        setIcon(url);
+        setUsername(user);
+        setUserRecipes(recipes);
+
+        dispatch(hideLoading());
+    }, []);
+
+    return (
+        <>{ username && 
+            <><h1 className="center title">{ username }'s Profile</h1>
+            <div className="user-profile-page">
+                
+                <div className="user-profile-page__left">
+                    <h2 className="title">User's Information</h2>
+                    <p>Username: { username }</p>
+                </div>
+
+                <div className="user-profile-page__center">
+                    <h2 className="title">{ username }'s Recipes</h2>
+                    { userRecipes && userRecipes.length < 1 && <p>This user hasn't submitted any recipes yet!</p>}
+                    { userRecipes && userRecipes.length > 0 && userRecipes.map((recipe) => (
+                        <li key={ recipe._id }>{ recipe.title }</li>
+                    ))}
+                </div>
+
+                <div className="user-profile-page__right">
+                    <img height="250" width="250" src={ icon } />
+                </div>
+            </div></>
+        }</>
+    )
+}
+
+export { UserProfilePage as default }
--- client/src/router/AppRouter.js
+++ client/src/router/AppRouter.js
@@ -7,11 +7,13 @@ import Nav from '../components/nav/Nav.js'; //nav
 // import AllRecipesPage from '../components/home/AllRecipesPage.js';
 // import AboutPage from '../components/home/AboutPage.js';
 // import ConversionsPage from '../components/home/ConversionsPage.js';
+// import UserProfilePage from '../components/home/UserProfilePage.js';
 // import ViewRecipePage from '../components/home/ViewRecipePage.js';
 const HomePage = React.lazy(() => import('../components/home/HomePage.js'));
 const AllRecipesPage = React.lazy(() => import('../components/home/AllRecipesPage.js'));
 const AboutPage = React.lazy(() => import('../components/home/AboutPage.js'));
 const ConversionsPage = React.lazy(() => import('../components/home/ConversionsPage.js'));
+const UserProfilePage = React.lazy(() => import('../components/home/UserProfilePage.js'));
 const ViewRecipePage = React.lazy(() => import('../components/home/ViewRecipePage.js'));
 //Account components/authorized only pages.
 // import EditRecipePage from '../components/account/EditRecipePage.js';
@@ -27,7 +29,7 @@ const SubmitRecipePage = React.lazy(() => import('../components/account/SubmitRe
 const AppRouter = () => (
     <Router>
         <Nav />
-        <React.Suspense fallback={<p className="center">Loading...</p>}>
+        <React.Suspense fallback={ <p className="center">Loading...</p> }>
             <Switch>
                 <Route exact path={'/'} component={HomePage} />
 
@@ -43,7 +45,9 @@ const AppRouter = () => (
 
                 <Route path={'/about'} component={ AboutPage } />
 
-                <Route path={`/recipe`} component={ ViewRecipePage } />
+                <Route path={'/recipe'} component={ ViewRecipePage } />
+
+                <Route path={'/user'} component={ UserProfilePage } />
 
                 <UserRoute path='/editrecipe' component={ EditRecipePage } />
 

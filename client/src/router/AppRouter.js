2386d2131cf352fbb309d768b3420e5e83d85f86
Evan <evan1715@users.noreply.github.com>
2021-06-14 21:18:26 -0400

    Separated the lazy load section from regular import to make it easier to switch between them.

--- client/src/router/AppRouter.js
+++ client/src/router/AppRouter.js
@@ -3,7 +3,9 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import UserRoute from './UserRoute.js'; //private routes
 import Nav from '../components/nav/Nav.js'; //nav
 import Footer from '../components/nav/Footer.js'; //footer
-// Home components/non-authorized pages.
+
+//Normal import method.
+//Home components/non-authorized pages.
 import HomePage from '../components/home/HomePage.js';
 import AllRecipesPage from '../components/home/AllRecipesPage.js';
 import AboutPage from '../components/home/AboutPage.js';
@@ -11,6 +13,14 @@ import ConversionsPage from '../components/home/ConversionsPage.js';
 import SearchResultsPage from '../components/home/SearchResultsPage.js';
 import UserProfilePage from '../components/home/UserProfilePage.js';
 import ViewRecipePage from '../components/home/ViewRecipePage.js';
+//Account components/authorized only pages.
+import EditRecipePage from '../components/account/EditRecipePage.js';
+import MyAccountPage from '../components/account/MyAccountPage.js';
+import MyRecipesPage from '../components/account/MyRecipesPage.js';
+import SubmitRecipePage from '../components/account/SubmitRecipePage.js';
+
+//Lazy Load method -- going to disable it for now since the project is small and lazy loading doesn't benefit much. We'll keep it speedy by having it all in one.
+// //Home components/non-authorized pages.
 // const HomePage = React.lazy(() => import('../components/home/HomePage.js'));
 // const AllRecipesPage = React.lazy(() => import('../components/home/AllRecipesPage.js'));
 // const AboutPage = React.lazy(() => import('../components/home/AboutPage.js'));
@@ -18,11 +28,7 @@ import ViewRecipePage from '../components/home/ViewRecipePage.js';
 // const SearchResultsPage = React.lazy(() => import('../components/home/SearchResultsPage.js'));
 // const UserProfilePage = React.lazy(() => import('../components/home/UserProfilePage.js'));
 // const ViewRecipePage = React.lazy(() => import('../components/home/ViewRecipePage.js'));
-//Account components/authorized only pages.
-import EditRecipePage from '../components/account/EditRecipePage.js';
-import MyAccountPage from '../components/account/MyAccountPage.js';
-import MyRecipesPage from '../components/account/MyRecipesPage.js';
-import SubmitRecipePage from '../components/account/SubmitRecipePage.js';
+// //Account components/authorized only pages.
 // const EditRecipePage = React.lazy(() => import('../components/account/EditRecipePage.js'));
 // const MyAccountPage = React.lazy(() => import('../components/account/MyAccountPage.js'));
 // const MyRecipesPage = React.lazy(() => import('../components/account/MyRecipesPage.js'));

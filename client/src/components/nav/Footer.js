08441c8a22c3eaaecd0122398483959d56ced1c3
Evan <evan1715@users.noreply.github.com>
2021-06-17 17:58:16 -0400

    Updated about and footer.

--- client/src/components/nav/Footer.js
+++ client/src/components/nav/Footer.js
@@ -1,12 +1,9 @@
 import React from 'react';
 
-const Footer = () => {
-
-    return (
-        <footer className="center">
-            Footer placeholder.
-        </footer>
-    )
-}
+const Footer = () => (
+    <footer className="center">
+        Website created by Evan McHugh.
+    </footer>
+);
 
 export { Footer as default }

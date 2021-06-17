b01fed1806ed382d75b9128b99115b53346b297c
Evan <evan1715@users.noreply.github.com>
2021-06-17 17:58:16 -0400

    Updated about and footer.

--- client/src/components/home/AboutPage.js
+++ client/src/components/home/AboutPage.js
@@ -4,8 +4,10 @@ const AboutPage = () => (
     <div className="about-container">
         <h1 className="center title">About</h1>
             <p className="center">
-                Content pending.
+                The website created by Evan McHugh. I designed this project to be a full-stack app using the MERN (MongoDB, Express, React, NodeJS) stack. 
+                For more information, check out the repository on GitHub!
             </p>
+            <a className="link-blue" href="https://github.com/evan1715/RecipeProject" target="_blank">View the repository</a>
     </div>
 );
 
